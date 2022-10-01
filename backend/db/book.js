f4b58cd583f4e655eb71cf2b915b6a6cc8a82039
AyumiMoriuchi <moriuchi.0930@gmail.com>
2022-10-01 16:54:58 +0900

    競合回避のため追加プルリク

--- backend/db/book.js
+++ backend/db/book.js
@@ -1,62 +1,56 @@
 const { STRING, DATE } = require("sequelize");
 
 module.exports. getBookModel = function (db) {
-<<<<<<< HEAD
     var BookModel = db.define(
-=======
-        var BookModel = db.define(
->>>>>>> a9d8a5cf8f7cc5871aea5004a2a1e5a0a639669f
-            "book",
-            {
-            isbn: {
-                field: "isbn",
-                type: STRING(13),
-                primaryKey: true,
-                allowNull: false
-            }, 
-            book_id: {
-                field: "book_id",
-                type: STRING(13),
-                primaryKey: true,
-                allowNull: false
-            }, 
-            title: {
-                field: "title",
-                type: STRING(100),
-                allowNull: false
-            },
-            description: {
-                field: "description",
-                type: STRING(1024),
-                allowNull: true
-            },
-            img_url: {
-                field: "img_url",
-                type: STRING(255),
-                allowNull: true
-            },
-            rental_status: {
-                field: "rental_status",
-                type: STRING(100),
-                allowNull: true
-            },
-            rental_user: {
-                field: "rental_user",
-                type: STRING(100),
-                allowNull: true
-            }},
-            {
-                createdAt: false, //デフォルト項目を生成しないように
-                updatedAt: false,  //デフォルト項目を生成しないように
-                tableName: "book" //明示的にテーブル名を指定
-            }
-        );
-        BookModel.associate = (db) => {
-            BookModel.belongsTo(db.lending);
-        };
-<<<<<<< HEAD
-        return BookModel;
+        "book",
+        {
+        isbn: {
+            field: "isbn",
+            type: STRING(13),
+            primaryKey: true,
+            allowNull: false
+        }, 
+        book_id: {
+            field: "book_id",
+            type: STRING(13),
+            primaryKey: true,
+            allowNull: false
+        }, 
+        title: {
+            field: "title",
+            type: STRING(100),
+            allowNull: false
+        },
+        description: {
+            field: "description",
+            type: STRING(1024),
+            allowNull: true
+        },
+        img_url: {
+            field: "img_url",
+            type: STRING(255),
+            allowNull: true
+        },
+        rental_status: {
+            field: "rental_status",
+            type: STRING(100),
+            allowNull: true
+        },
+        rental_user: {
+            field: "rental_user",
+            type: STRING(100),
+            allowNull: true
+        }},
+        {
+            createdAt: false, //デフォルト項目を生成しないように
+            updatedAt: false,  //デフォルト項目を生成しないように
+            tableName: "book" //明示的にテーブル名を指定
+        }
+    );
+
+    BookModel.associate = (db) => {
+        BookModel.belongsTo(db.lending);
+    };
+
+    return BookModel;
 }
-=======
-        return BookModel;}
->>>>>>> a9d8a5cf8f7cc5871aea5004a2a1e5a0a639669f
