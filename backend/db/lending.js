8e350921c98d70cda3fc6598910265ccbed40938
NishidaR <nishi.dg98@gmail.com>
2022-10-15 09:31:39 +0900

    * マージミス修正

--- backend/db/lending.js
+++ backend/db/lending.js
@@ -4,45 +4,46 @@ module.exports. getLendingModel = function (db) {
     var lending = db.define(
         "lending",
         {
-        lendingUserId: {
-            field: "lending_user_id",
-            type: STRING(16),
-            primaryKey: true,
-            allowNull: false
+            lending_user_id: {
+                field: "lending_user_id",
+                type: STRING(16),
+                primaryKey: true,
+                allowNull: false
+            },
+            isbn: {
+                field: "isbn",
+                type: STRING(13),
+                primaryKey: true,
+                allowNull: false
+            },
+            book_id: {
+                field: "book_id",
+                type: STRING(13),
+                primaryKey: true,
+                allowNull: false
+            },
+            rental_date: {
+                field: "rental_date",
+                type: DATE,
+                primaryKey: true,
+                allowNull: false
+            },
+            managed_user_id: {
+                field: "managed_user_id",
+                type: STRING(16),
+                allowNull: false
+            },
+            return_plan_date: {
+                field: "return_plan_date",
+                type: DATE,
+                allowNull: false
+            },
+            return_act_date: {
+                field: "return_act_date",
+                type: DATE,
+                allowNull: true
+            }
         },
-        isbn: {
-            field: "isbn",
-            type: STRING(13),
-            primaryKey: true,
-            allowNull: false
-        },
-        bookId: {
-            field: "book_id",
-            type: STRING(13),
-            primaryKey: true,
-            allowNull: false
-        },
-        rentalDate: {
-            field: "rental_date",
-            type: DATE,
-            primaryKey: true,
-            allowNull: false
-        },
-        managedUserId: {
-            field: "managed_user_id",
-            type: STRING(16),
-            allowNull: false
-        },
-        returnPlanDate: {
-            field: "return_plan_date",
-            type: DATE,
-            allowNull: false
-        },
-        returnActDate: {
-            field: "return_act_date",
-            type: DATE,
-            allowNull: true
-        }},
         {
             createdAt: false, //デフォルト項目を生成しないように
             updatedAt: false,  //デフォルト項目を生成しないように
