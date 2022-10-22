f66e9bda581dcb159c36f7599881b82006ef1499
AyumiMoriuchi <moriuchi.0930@gmail.com>
2022-10-22 17:34:43 +0900

    貸出状況確認画面、ブックメンテナンス修正

--- backend/index.js
+++ backend/index.js
@@ -187,25 +187,6 @@ app.delete("/api/user/:userId", function(req, res) {
 });
 
 /**
- * ユーザ名に紐づく書籍情報取得API
- */
- app.get("/api/book/:userName", function(req, res) {
-    // 書籍取得する
-    BookLogic.getLinkUser(db, req.params.userName)
-        .then((books) => {
-            // 正常レスポンス
-            res.send({
-                Items: JSON.stringify(books)
-            });
-        })
-        .catch(()  => {
-            // 異常レスポンス
-            console.log("failed to get book");
-            res.status(500).send("server error occur")
-        });
-});
-
-/**
  * 貸し出し状況登録API
  */
  app.post("/api/lending", function(req, res) {
