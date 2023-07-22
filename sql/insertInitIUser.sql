dc56b844052ecc19b2dea77cfae01395b7b9ef06
NishidaR <nishi.dg98@gmail.com>
2023-07-22 12:29:25 +0900

    単体テスト用にテストユーザーの投入データを修正

--- sql/insertInitIUser.sql
+++ sql/insertInitIUser.sql
@@ -1,4 +1,5 @@
 USE intern;
 DELETE FROM user;
-INSERT INTO user values ('intern01', 'インターンユーザー1', 'intern01', '1','1');
-INSERT INTO user values ('intern02', 'インターンユーザー2', 'intern02', '2','2');
+INSERT INTO user values ('user0001', 'ユーザー0001', 'intern01', '1','2');
+INSERT INTO user values ('intern02', '一般', 'intern02', '1','1');
+INSERT INTO user values ('intern03', '管理者', 'intern03', '2','2');
