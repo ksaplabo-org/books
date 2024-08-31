7a8a590bea456f732585c591a892e9cf6fbc7dfe
TakeTakeGomaKonatsu <yurie.takehara@tcs-ipnet.co.jp>
2024-08-31 10:57:40 +0900

    初期データ投入スクリプトの修正 * 初期データ投入をまとめたスクリプトを追加
    # Conflicts:
    #	sql/insertInitIUser.sql
    #	sql/insertInitStudent.sql

--- sql/insertInitStudent.sql
+++ sql/insertInitStudent.sql
@@ -1,11 +1,8 @@
 use intern;
 
 delete from student;
-insert into student values ('10000001','成田', '帆乃夏');
-insert into student values ('10000002','伊藤', '慎吾');
-insert into student values ('10000003','中村', '汐那');
-insert into student values ('10000004','富岡', '賢');
-insert into student values ('10000005','本崎', '悠仁');
-insert into student values ('10000006','小原', '哉太');
-insert into student values ('10000007','大島', '楓也');
-insert into student values ('10000008','月江', '惇元');
+insert into student values ('10000001','山田', '太郎');
+insert into student values ('10000002','鈴木', '次郎');
+insert into student values ('10000003','中村', '三太');
+insert into student values ('10000004','渡辺', '花子');
+insert into student values ('10000005','小林', '栄子');
