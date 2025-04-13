390cff950eed929ce92975a1777392484f79523e
KotaNakase <nakko1130@gmail.com>
2025-04-13 13:19:38 +0900

    frontendのホットリロード有効化

--- front/vue.config.js
+++ front/vue.config.js
@@ -11,88 +11,92 @@ module.exports = {
     sourceMap: true,
   },
   lintOnSave: true,
-    pages: {
-        signin: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/signin.html',
-          title: 'サインイン',
-        },
-        signout: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/signout.html',
-          title: 'サインアウト',
-        },
-        top: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/top.html',
-          title: 'トップページ',
-        },
-        rentalbook: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/rentalbook.html',
-          title: '貸出状況登録',
-        },
-        maintebook: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/maintebook.html',
-          title: '書籍管理',
-        },
-        listbook: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/listBook.html',
-          title: '書籍一覧',
-        },
-        listUser: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/listUser.html',
-          title: 'ユーザー一覧',
-        },
-        leadingBook: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/leadingBook.html',
-          title: '貸出状況一覧',
-        },
-        addUser: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/addUser.html',
-          title: 'ユーザー追加',
-        },
-        editUser: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/editUser.html',
-          title: 'ユーザー管理',
-        },
-        student: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/student.html',
-          title: '学生課題',
-        },
-        sorry: {
-          entry: 'src/main.js',
-          template: 'public/pages/index.html',
-          filename: 'pages/sorry.html',
-          title: 'Sorry',
-        },
-      },
-    devServer: {
-      https: true,
-      port: 8080,
-      proxy: {
-          '/api': {
-              target: "http://api:3000",
-              changeOrigin: true,
-          }
+  pages: {
+    signin: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/signin.html',
+      title: 'サインイン',
+    },
+    signout: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/signout.html',
+      title: 'サインアウト',
+    },
+    top: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/top.html',
+      title: 'トップページ',
+    },
+    rentalbook: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/rentalbook.html',
+      title: '貸出状況登録',
+    },
+    maintebook: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/maintebook.html',
+      title: '書籍管理',
+    },
+    listbook: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/listBook.html',
+      title: '書籍一覧',
+    },
+    listUser: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/listUser.html',
+      title: 'ユーザー一覧',
+    },
+    leadingBook: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/leadingBook.html',
+      title: '貸出状況一覧',
+    },
+    addUser: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/addUser.html',
+      title: 'ユーザー追加',
+    },
+    editUser: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/editUser.html',
+      title: 'ユーザー管理',
+    },
+    student: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/student.html',
+      title: '学生課題',
+    },
+    sorry: {
+      entry: 'src/main.js',
+      template: 'public/pages/index.html',
+      filename: 'pages/sorry.html',
+      title: 'Sorry',
+    },
+  },
+  devServer: {
+    https: true,
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: "http://api:3000",
+        changeOrigin: true,
       }
+    },
+    watchOptions: {
+      ignored: /node_modules/,
+      poll: true
+    }
   }
 };
