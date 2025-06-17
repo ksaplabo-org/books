1a8de57efa1e9be5c62cbeea5e28b5e61eb67499
NishidaR <nishi.dg98@gmail.com>
2025-06-17 23:56:44 +0900

    コード改善
    * 問題用ファイルの格納先をbooks配下に変更
    * パス、メニュー系のファイルを問題用ファイルに追加

---
+++ intern/front/vue.config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  publicPath: "/public",
+  outputDir: "dist",
+  assetsDir: "assets",
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  parallel: true,
+  css: {
+    modules: false,
+    extract: true,
+    sourceMap: true,
+  },
+  lintOnSave: true,
+  pages: {
+    signIn: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/sign-in.html",
+      title: "サインイン",
+    },
+    signOut: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/sign-out.html",
+      title: "サインアウト",
+    },
+    top: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/top.html",
+      title: "トップページ",
+    },
+    bookList: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/book/list.html",
+      title: "書籍一覧",
+    },
+    bookManager: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/book/manager.html",
+      title: "書籍管理",
+    },
+    userList: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/user/list.html",
+      title: "ユーザー一覧",
+    },
+    userCreate: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/user/create.html",
+      title: "ユーザー追加",
+    },
+    userManager: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/user/manager.html",
+      title: "ユーザー管理",
+    },
+    rentalBook: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/book/rental.html",
+      title: "レンタル",
+    },
+    rentalList: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/book/rental/list.html",
+      title: "レンタル一覧",
+    },
+    student: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/student.html",
+      title: "学生課題",
+    },
+    sorry: {
+      entry: "src/main.js",
+      template: "public/pages/index.html",
+      filename: "pages/sorry.html",
+      title: "Sorry",
+    },
+  },
+  devServer: {
+    https: true,
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+};
