3bd07d6eaad1d7049728bc3b6d244b0a50e613f9
NishidaR <nishi.dg98@gmail.com>
2025-07-04 21:00:05 +0900

    initial commit

---
+++ backend/logic/book.js
@@ -0,0 +1,123 @@
+// moment import
+const moment = require("moment");
+const BookRepository = require("../db/book");
+
+/**
+ * 書籍情報を全件検索
+ *
+ * @param {*} db
+ * @returns {Promise<Object[]>}
+ */
+module.exports.getAll = async function (db) {
+  const BookModel = BookRepository.getBookModel(db);
+
+  try {
+    return await BookModel.findAll();
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * 書籍情報を検索
+ *
+ * [検索条件]
+ * 書籍名の部分一致
+ *
+ * @param {*} db
+ * @param {*} searchWord
+ * @returns {Promise<Object[]>}
+ */
+module.exports.getAllSearchBooks = async function (db, searchWord) {
+  const BookModel = BookRepository.getBookModel(db);
+  const Sequelize = require("sequelize");
+  const Op = Sequelize.Op;
+
+  try {
+    return await BookModel.findAll({
+      where: {
+        title: {
+          [Op.like]: "%" + searchWord + "%",
+        },
+      },
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * 書籍情報を新規登録
+ *
+ * @param {*} db
+ * @param {*} isbn
+ * @param {*} bookId
+ * @param {*} title
+ * @param {*} description
+ * @param {*} imgUrl
+ * @returns {Promise<void>}
+ */
+module.exports.add = async function (db, isbn, bookId, title, description, imgUrl) {
+  const BookModel = BookRepository.getBookModel(db, book);
+
+  try {
+    return await BookModel.create({
+      isbn: isbn,
+      book_id: bookId,
+      title: title,
+      description: description,
+      img_url: imgUrl,
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * 書籍情報を更新
+ *
+ * @param {*} db
+ * @param {*} title
+ * @param {*} isUpdateRental
+ * @returns {Promise<void>}
+ */
+module.exports.updateState = async function (db, title, userName, isUpdateRental) {
+  const BookModel = BookRepository.getBookModel(db);
+
+  try {
+    const updateParams = {
+      rentalStatus: null,
+      rentalDate: null,
+      returnDate: null,
+      rentalUser: null,
+    };
+
+    if (isUpdateRental) {
+      updateParams.rentalStatus = "貸出中";
+      updateParams.rentalDate = moment().format("YYYY/MM/DD");
+      updateParams.returnDate = moment().add(2, "w").format("YYYY/MM/DD");
+      updateParams.rentalUser = userName;
+    }
+
+    return await BookModel.update(updateParams, { where: { title: title } });
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * 書籍情報を削除
+ *
+ * @param {*} db
+ * @param {*} title
+ * @returns {Promise<void>}
+ */
+module.exports.remove = async function (db, title) {
+  const BookModel = BookRepository.getBookModel(db);
+
+  try {
+    return await BookModel.destroy({ where: { title: title } });
+  } catch (e) {
+    throw e;
+  }
+};
